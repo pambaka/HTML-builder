5e2cba10919e5dc9c69ba4635195545b3ec54dc1
pambaka <pambaka@ya.ru>
2024-01-20 13:46:26 +0100

    feat: files-in-folder is implemented

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const folder = 'secret-folder';
+
+fs.readdir(path.join(__dirname, folder), {withFileTypes: true}, (err, entries) => { 
+  entries.forEach(entry => { 
+    if (entry.isFile()) {
+      const fileName = entry.name;
+      const fileExtension = path.extname(fileName).replace('.', '');
+
+      fs.stat(path.join(__dirname, folder, fileName), (err, stats) => {
+        const fileSize = `${stats.size / 1000}kB`;
+        const outputStr = `${fileName} - ${fileExtension} - ${fileSize}`;
+
+        console.log(outputStr);
+      });
+    }
+  });
+});
