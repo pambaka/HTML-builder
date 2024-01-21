83a413715c26cb32402b900606a71d5eaf029176
pambaka <pambaka@ya.ru>
2024-01-21 14:49:09 +0100

    feat: merge-styles is implemented

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesFolder = 'styles';
+const projectFolder = 'project-dist';
+const bundleCssFile = 'bundle.css';
+
+fs.promises
+  .readdir(path.join(__dirname, stylesFolder), { withFileTypes: true })
+  .then((entries) => {
+    const stylesArr = [];
+
+    entries.forEach((entry) => {
+      const filePath = path.join(__dirname, stylesFolder, entry.name);
+
+      if (entry.isFile() && path.extname(filePath) === '.css') {
+        const readStream = fs.createReadStream(
+          path.join(__dirname, stylesFolder, entry.name),
+          { encoding: 'utf-8' },
+        );
+        const writeStream = fs.createWriteStream(
+          path.join(__dirname, projectFolder, bundleCssFile),
+        );
+
+        const promise = new Promise(function (resolve, reject) {
+          readStream.on('data', (data) => {
+            stylesArr.push(data);
+
+            resolve(stylesArr);
+          });
+        });
+
+        promise.then((array) => {
+          writeStream.write(array.join(''));
+        });
+      }
+    });
+  });
