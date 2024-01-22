97276b4c15d72ae80942b7d57943536d3fe930a5
pambaka <pambaka@ya.ru>
2024-01-22 15:47:24 +0100

    fix: fix write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const rl = require('readline');
+const path = require('path');
 
 const input = process.stdin;
 const output = process.stdout;
 const readLine = rl.createInterface({ input, output });
 
-const writeStream = fs.createWriteStream('./02-write-file/text.txt');
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
-readLine.write(`Please type smth in: \n`)
+readLine.write(`Please type smth in: \n`);
 
 readLine.on('line', (data) => {
   if (data === 'exit') {
     handleExit();
+  } else {
+    readLine.write(writeStream.write(`${data}\n`));
   }
-
-  readLine.write(writeStream.write(`${data}\n`));
-})
+});
 
 readLine.on('SIGINT', handleExit);
 
@@ -23,5 +24,5 @@ function handleExit() {
   readLine.write('Stop writting to file');
   readLine.close();
 
-  process.exit();
+  // process.exit(0);
 }
