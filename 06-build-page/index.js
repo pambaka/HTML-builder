c95f2e043dc40e9973aacf558e69f2b3e8510627
pambaka <pambaka@ya.ru>
2024-01-22 20:38:00 +0100

    refactor: refactor build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,29 +6,33 @@ const projectFolder = 'project-dist';
 buildPage();
 
 async function buildPage() {
-  const existsPromise = new Promise ((resolve, reject) => {
+  const existsPromise = new Promise((resolve, reject) => {
     fs.access(path.join(__dirname, projectFolder), (err) => {
       const exists = !Boolean(err);
 
       resolve(exists);
     });
-  })
+  });
   const exists = await existsPromise;
   console.log('Directory exists:', exists);
 
-  const dirPromise = new Promise ((resolve, reject) => {
+  const dirPromise = new Promise((resolve, reject) => {
     if (!exists) {
       fs.mkdir(path.join(__dirname, projectFolder), (err, data) => {});
 
       resolve(`${projectFolder} directory is created`);
     } else {
       async function deleteFiles() {
-        const entries = await fs.promises.readdir(path.join(__dirname, projectFolder));
+        const entries = await fs.promises.readdir(
+          path.join(__dirname, projectFolder),
+        );
 
         console.log(`Files in directory: ${entries}`);
 
         for (const entry of entries) {
-          await fs.promises.rm(path.join(__dirname, projectFolder, entry), {recursive: true});
+          await fs.promises.rm(path.join(__dirname, projectFolder, entry), {
+            recursive: true,
+          });
 
           console.log(`Delete ${entry}`);
         }
@@ -38,51 +42,60 @@ async function buildPage() {
 
       deleteFiles();
     }
-  })
+  });
   const dirMessage = await dirPromise;
   console.log(dirMessage);
- 
+
   createHtmlFile();
   createStyleFile();
 
   const assetsFolder = 'assets';
-  copyFiles(path.join(__dirname, assetsFolder), path.join(__dirname, projectFolder, assetsFolder));
+  copyFiles(
+    path.join(__dirname, assetsFolder),
+    path.join(__dirname, projectFolder, assetsFolder),
+  );
 }
 
 function createHtmlFile() {
-  const readStream = fs.createReadStream(path.join(__dirname, 'template.html'), {encoding: 'utf-8'});
-  const writeStream = fs.createWriteStream(path.join(__dirname, projectFolder, 'index.html'));
-  
-  const componenetsFolder = 'components';
-  
+  const readStream = fs.createReadStream(
+    path.join(__dirname, 'template.html'),
+    { encoding: 'utf-8' },
+  );
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, projectFolder, 'index.html'),
+  );
+
   readStream.on('data', (data) => {
     console.log('Writing index.html');
     
+    const componenetsFolder = 'components';
     const tags = [];
 
     fs.readdir(path.join(__dirname, componenetsFolder), (err, entries) => {
-      entries.forEach(entry => {
-       
-        tags.push(entry);  
+      entries.forEach((entry) => {
+        tags.push(entry);
       });
 
       for (let i = 0; i < tags.length; i += 1) {
         const str = `{{${tags[i].replace('.html', '')}}}`;
 
-        const stream = fs.createReadStream(path.join(__dirname, componenetsFolder, tags[i]), {encoding: 'utf-8'});
-        
+        const stream = fs.createReadStream(
+          path.join(__dirname, componenetsFolder, tags[i]),
+          { encoding: 'utf-8' },
+        );
+
         stream.on('data', (htmlData) => {
           data = data.replace(str, htmlData);
-          
+
           if (i === tags.length - 1) {
             writeStream.write(data);
           }
         });
       }
-    })  
-  })
+    });
+  });
 }
-  
+
 function createStyleFile() {
   console.log('Bundling style.css');
 
@@ -122,15 +135,18 @@ function createStyleFile() {
       });
     });
 }
-  
+
 function copyFiles(srcFolder, destFolder) {
   console.log(`Copying ${destFolder}`);
-  
+
   fs.promises.mkdir(destFolder).then(() => {
-    fs.readdir(srcFolder, {withFileTypes: true}, (err, entries) => {
+    fs.readdir(srcFolder, { withFileTypes: true }, (err, entries) => {
       entries.forEach((entry) => {
         if (!entry.isFile()) {
-          copyFiles(path.join(srcFolder, entry.name), path.join(destFolder, entry.name));
+          copyFiles(
+            path.join(srcFolder, entry.name),
+            path.join(destFolder, entry.name),
+          );
         } else {
           fs.copyFile(
             path.join(srcFolder, entry.name),
