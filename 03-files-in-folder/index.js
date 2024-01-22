a19d045de56f755c351c8fe06c66d2f8fd052318
pambaka <pambaka@ya.ru>
2024-01-22 16:37:57 +0100

    fix: fix files-in-folder file size appearance

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,18 +3,22 @@ const path = require('path');
 
 const folder = 'secret-folder';
 
-fs.readdir(path.join(__dirname, folder), {withFileTypes: true}, (err, entries) => { 
-  entries.forEach(entry => { 
-    if (entry.isFile()) {
-      const fileName = entry.name;
-      const fileExtension = path.extname(fileName).replace('.', '');
+fs.readdir(
+  path.join(__dirname, folder),
+  { withFileTypes: true },
+  (err, entries) => {
+    entries.forEach((entry) => {
+      if (entry.isFile()) {
+        const fileName = entry.name;
+        const fileExtension = path.extname(fileName).replace('.', '');
 
-      fs.stat(path.join(__dirname, folder, fileName), (err, stats) => {
-        const fileSize = `${stats.size / 1000}kB`;
-        const outputStr = `${fileName} - ${fileExtension} - ${fileSize}`;
+        fs.stat(path.join(__dirname, folder, fileName), (err, stats) => {
+          const fileSize = `${stats.size / 1000}KB`;
+          const outputStr = `${fileName} - ${fileExtension} - ${fileSize}`;
 
-        console.log(outputStr);
-      });
-    }
-  });
-});
+          console.log(outputStr);
+        });
+      }
+    });
+  },
+);
