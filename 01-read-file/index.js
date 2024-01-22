3457a9e1538d89b920bffd9aa20d958aaf400408
pambaka <pambaka@ya.ru>
2024-01-22 15:32:44 +0100

    fix: fix path in read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
-const readStream = fs.createReadStream('./01-read-file/text.txt', {encoding: 'utf-8'});
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
+  encoding: 'utf8',
+});
 
 readStream.on('data', (data) => {
   console.log(data);
